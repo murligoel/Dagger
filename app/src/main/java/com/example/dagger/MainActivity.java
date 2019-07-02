4b91d7f15feda6d2708c9e4f9683ada97d619a80
murligoel <murli.goel1999@gmail.com>
2019-07-02 11:51:41 +0530

    added field injection by passing the fields and annotating the variable with @Inject instead of private

--- app/src/main/java/com/example/dagger/MainActivity.java
+++ app/src/main/java/com/example/dagger/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.dagger;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
-    private Car car;
+    @Inject Car car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         CarComponent component = DaggerCarComponent.create();
-        car = component.getCar();
+        component.inject(this);
         car.drive();
     }
 }
