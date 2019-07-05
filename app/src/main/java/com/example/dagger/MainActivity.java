3fd5bc962263460ebd69c4d2846c3ba9c3fbcdd6
murligoel <murli.goel1999@gmail.com>
2019-07-05 15:29:00 +0530

    created new activity to work on dagger with shared preference i.e saving the data for the future reference

--- app/src/main/java/com/example/dagger/MainActivity.java
+++ app/src/main/java/com/example/dagger/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.dagger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.dagger.car.Car;
 import com.example.dagger.dagger.CarComponent;
 import com.example.dagger.dagger.DaggerCarComponent;
-import com.example.dagger.dagger.DieselEngineModule;
 
 import javax.inject.Inject;
 
@@ -14,10 +16,21 @@ public class MainActivity extends AppCompatActivity {
     @Inject
     Car car;
 
+    private Button button;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button_new);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,NewActivity.class));
+            }
+        });
 
         CarComponent component = DaggerCarComponent.builder()
                 .horsePower(150)
